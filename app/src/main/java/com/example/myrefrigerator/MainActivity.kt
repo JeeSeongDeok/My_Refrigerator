9c09f95497d5103e8c6cbd460ce0244bfbf18910
HeyDosomething <bh939@naver.com>
2021-07-07 20:17:11 +0900

    2021.07.07

--- app/src/main/java/com/example/myrefrigerator/MainActivity.kt
+++ app/src/main/java/com/example/myrefrigerator/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myrefrigerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        // ↓툴바에 홈버튼을 활성화 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+    //액션버튼 메뉴 액션바에 집어 넣기
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
 }
