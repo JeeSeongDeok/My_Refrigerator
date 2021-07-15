e570e1d9d2faa1b6cf74457c562a1cfab71d6a88
tmddmddnjs <stratos97@naver.com>
2021-07-15 16:01:11 +0900

    2021.07.15_tmddmddnjs

--- app/src/main/java/com/example/myrefrigerator/views/SignupActivity.kt
+++ app/src/main/java/com/example/myrefrigerator/views/SignupActivity.kt
@@ -20,43 +20,58 @@ class SignupActivity : AppCompatActivity(), View.OnClickListener {
     // Viewmodel
     private val mySignupViewModel = SignupViewModel()
     // Binding
-    private lateinit var binding: ActivitySignupBinding
-    //private val binding get() = mBinding!!
+    private var mBinding: ActivitySignupBinding? = null
+    // mBinding이 null이 아닌 경우 get
+    private val binding get() = mBinding!!
     companion object {
         const val TAG: String = "로그"
     }
+    //달력선택
+    var today = GregorianCalendar()
+    var year: Int = today.get(Calendar.YEAR)
+    var month: Int = today.get(Calendar.MONTH)
+    var date: Int = today.get(Calendar.DATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // XML 연결
-        binding = DataBindingUtil.setContentView<ActivitySignupBinding>(this, R.layout.activity_signup)
-        binding.su = SignupActivity()
+        mBinding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Log
         Log.e(TAG, "SignupActivity - onCreate Call()")
+        // setListener
+        binding.signup.setOnClickListener(this)
+        binding.overlap.setOnClickListener(this)
+        binding.birthBtn.setOnClickListener(this)
     }
 
     fun blank_edit() {
-        if (binding.id.toString().equals(""))
-            Log.d(SignupActivity.TAG, "id채워지지 않음")
-        else if (binding.pass.toString().equals(""))
+        if (binding.id.length() == 0)
+            Toast.makeText(this@SignupActivity, "아이디를 채워주세요.", Toast.LENGTH_SHORT).show()
+        else if (binding.pass.length() == 0)
             Toast.makeText(this@SignupActivity, "비밀번호를 채워주세요.", Toast.LENGTH_SHORT).show()
-        else if (binding.repass.toString().equals(""))
+        else if (binding.repass.length() == 0)
             Toast.makeText(this@SignupActivity, "비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
-        else if (binding.username.toString().equals(""))
+        else if (binding.username.length() == 0)
             Toast.makeText(this@SignupActivity, "이름을 채워주세요.", Toast.LENGTH_SHORT).show()
-        else if (binding.phonenumber.toString().equals(""))
+        else if (binding.phonenumber.length() == 0)
             Toast.makeText(this@SignupActivity, "전화번호를 채워주세요.", Toast.LENGTH_SHORT).show()
-        else if (binding.birth.toString().equals(""))
+        else if (binding.birth.length() == 0)
             Toast.makeText(this@SignupActivity, "생년월일을 채워주세요.", Toast.LENGTH_SHORT).show()
-        else if (binding.radioMan.isChecked == false || binding.radioWoman.isChecked == false)
+        //둘 중 하나만
+        else if (binding.radioMan.isChecked == false && binding.radioWoman.isChecked == false)
             Toast.makeText(this@SignupActivity, "성별칸을 채워주세요.", Toast.LENGTH_SHORT).show()
-        //대 채워졌을 때
+        //세개 다 채워야
+        else if (binding.agree1.isChecked == false || binding.agree2.isChecked == false ||
+                binding.agree3.isChecked == false)
+            Toast.makeText(this@SignupActivity, "이용약관에 동의해주세요.", Toast.LENGTH_SHORT).show()
+        //모든 칸이 채워졌을 때
         else {
             mySignupViewModel.sendSignUpInfo("쿼리문", 3)
         }
     }
 
-    // 클릭 리스너
+    // 클릭 리스너1
     override fun onClick(v: View?) {
         when (v) {
             // Signup 버튼을 눌렀을 경우
@@ -64,13 +79,12 @@ class SignupActivity : AppCompatActivity(), View.OnClickListener {
                 Log.d(SignupActivity.TAG, "LoginActivity - onCreate Call()")
                 blank_edit()
             }
-            //생년월일 선택
+            //중복확인
+            binding.overlap->{
+                Toast.makeText(this@SignupActivity, "중복확인", Toast.LENGTH_SHORT).show()
+            }
+            //달력선택
             binding.birthBtn -> {
-                var today = GregorianCalendar()
-                var year: Int = today.get(Calendar.YEAR)
-                var month: Int = today.get(Calendar.MONTH)
-                var date: Int = today.get(Calendar.DATE)
-
                 val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
                     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
                         binding.birth.setText("${year}년 ${month+1}월 ${dayOfMonth}일")
@@ -81,3 +95,4 @@ class SignupActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 }
+
