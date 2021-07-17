5f1624e2d29fbc13af2fc9bf59b9acc6a309887a
HeyDosomething <bh939@naver.com>
2021-07-17 18:20:54 +0900

    2021.07.17_Okhttp3_Retrofit

--- app/src/main/java/com/example/myrefrigerator/model/RetrofitInterface.kt
+++ app/src/main/java/com/example/myrefrigerator/model/RetrofitInterface.kt
@@ -2,9 +2,10 @@ package com.example.myrefrigerator.model
 
 import okhttp3.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RetrofitInterface{
     @GET("todos/1/")
-    fun getTodos() : Call<myData>
+    fun getTodos(@Path("user") userId : String,
+    id : String) : Call<TodosItem>
 }
-
