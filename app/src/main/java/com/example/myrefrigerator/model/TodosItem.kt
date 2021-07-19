6f88a1253a493102ac862199f319e196f27ed015
HeyDosomething <bh939@naver.com>
2021-07-19 21:54:53 +0900

    2021.07.19_retrofit2_fixv1

--- app/src/main/java/com/example/myrefrigerator/model/TodosItem.kt
+++
@@ -1,3 +0,0 @@
-package com.example.myrefrigerator.model
-
-data class TodosItem(val userId : Int, val id : Int, val title : String, val completed : Boolean)
--- app/src/main/java/com/example/myrefrigerator/views/testActivity.kt
+++ app/src/main/java/com/example/myrefrigerator/views/testActivity.kt
@@ -1,42 +1,54 @@
 package com.example.myrefrigerator.views
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myrefrigerator.R
-import com.example.myrefrigerator.databinding.ActivityMainBinding
 import com.example.myrefrigerator.databinding.ActivityTestBinding
-import com.example.myrefrigerator.model.Okhttp3RetrofitManager
-import com.example.myrefrigerator.model.Okhttp3RetrofitManager.getRetrofitService
-import com.example.myrefrigerator.model.RetrofitInterface
-import com.example.myrefrigerator.model.TodosItem
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.Response
+import com.example.myrefrigerator.model.RetrofitBuilder
+import com.example.myrefrigerator.model.ExampleInterface
+import com.example.myrefrigerator.model.Student
+import com.example.myrefrigerator.model.exampleResponse
+import retrofit2.Call;
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class testActivity : AppCompatActivity() {
+    val schoolId = 10102
+    val grade = 2
+    val classroom = 4
+
     private lateinit var mBinding: ActivityTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
-        val retClient = getRetrofitService(RetrofitInterface::class.java)
-        val retrofit = retClient.getTodos()
-        retrofit.enqueue(object : Callback<TodosItem> {
-            override fun onFailure(call: Call<TodosItem>, t: Throwable) {
-                t.printStackTrace()
-            }
-            override fun onResponse(call: Call<TodosItem>, response: Response<TodosItem>) {
-                if(response.body()!=null){
-                    val userId = "userId : ${response.body()?.userId}"
-                    val id = "id : ${response.body()?.id}"
-                    val title = "title : ${response.body()?.title}"
-                    val completed = "completed : ${response.body()?.completed}"
-                    mBinding.tvTodoId.text = id
-                    mBinding.tvTodoCompleted.text = title
+
+        // RetrofitBuilder를 준비함(http 연결까지 담당
+        val call = RetrofitBuilder.ConnetService
+        // getStudent를 불러 어떤 형식인지 부른 후 enqueue 실행
+        // enqueue는 exampleResponse(데이터클래스에 정의된 List<Student>)안에 데이터를 넣음
+        call.getStudent(schoolId, grade, classroom).enqueue(object : Callback<exampleResponse>{
+            override fun onResponse(call: Call<exampleResponse>, response: Response<exampleResponse>
+            ) {
+                // response.isSuccessful()은 연결도 성공하고, 문법도 잘 실행한 경우 발생
+                // http 응답코드는 200, 성공 했다는 뜻이다.
+                if(response.isSuccessful()) {
+                    // 성공 처리
+                }
+                // 연결은 성공했지만 문법이 틀린 경우
+                // http 응답코드는 400, 연결만 성공하고 요청은 실패함.
+                else {
+                    // 실패 처리
                 }
             }
+
+            override fun onFailure(call: Call<exampleResponse>, t: Throwable) {
+                // 연결 실패한 구간
+                // http 응답코드는 500이며, 응답을 실패했다는 뜻이다.
+            }
+
         })
+
     }
 }
