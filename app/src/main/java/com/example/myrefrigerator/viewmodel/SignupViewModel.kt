dd976be058059f8504a00f6e1f6e4e2b96fa2ef6
HeyDosomething <bh939@naver.com>
2021-07-25 23:49:16 +0900

    2021.07.25 Retrofit2 POST, GET

--- app/src/main/java/com/example/myrefrigerator/viewmodel/SignupViewModel.kt
+++ app/src/main/java/com/example/myrefrigerator/viewmodel/SignupViewModel.kt
@@ -2,8 +2,9 @@ package com.example.myrefrigerator.views
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.myrefrigerator.data.UserProfile
 import com.example.myrefrigerator.model.RetrofitBuilder
-import com.example.myrefrigerator.model.UserInfo
+import com.example.myrefrigerator.data.signupResult
 
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,21 +18,29 @@ class SignupViewModel : ViewModel() {
     init{
         Log.d(TAG, "SingupViewModel - called()")
     }
-    fun sendSignUpInfo(query: String, age:Int){
+    fun sendSignUpInfo(id: String, pass:String, name:String, phone:String, birth:String, gender:Int){
         // 연결하는 부분 OkHttp3 써서 백엔드랑 연결
         Log.d(TAG, "SingupViewModel - Call OkHttp3")
+        // Post할 데이터들
+        val data = UserProfile(id, pass, name, phone, "birth")
+        Log.d(TAG, "SingupViewModel - id:" + id)
 
-        val call = RetrofitBuilder.ConnetService
 
-        call.getUser().enqueue(object: Callback<UserInfo>{
-            override fun onFailure(call: Call<UserInfo>, t: Throwable) {
+        val call = RetrofitBuilder.CONNET_SERVICE
+
+
+        call.signUp(data).enqueue(object: Callback<signupResult>{
+            override fun onFailure(call: Call<signupResult>, t: Throwable) {
+                // 연결 실패한 경우
                 Log.e(TAG, "실패 에러: ${t.message}" )
             }
 
-            override fun onResponse(call: Call<UserInfo>, response: Response<UserInfo>) {
+            override fun onResponse(call: Call<signupResult>, response: Response<signupResult>) {
+                // 연결 성공한 경우
                 Log.e(TAG, response.body().toString() +"와 성공")
             }
 
         })
+
     }
 }
